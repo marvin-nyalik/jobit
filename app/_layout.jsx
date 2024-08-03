d0f4d39be264e32e2db2215268fce629f710b6c9
MARVIN <o.silverygold@gmail.com>
2024-08-03 17:28:24 +0300

    Define layout, load custom fonts, manage splashScreen

---
+++ app/_layout.jsx
@@ -0,0 +1,25 @@
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  const [fontsLoaded, error] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf")
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack />;
+};
+
+export default Layout;
