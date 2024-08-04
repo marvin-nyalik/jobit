103dd235804f0d50e56bf20ede2dc5d944f048a2
MARVIN <o.silverygold@gmail.com>
2024-08-04 18:56:55 +0300

    Complete PopularJobs wrapper component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -13,23 +13,24 @@ import useFetch from "../../../hook/useFetch";
 import styles from "./popularjobs.style";
 
 const Popularjobs = () => {
-  const [selectedJob, setSelectedJob] = useState();
+  const { data, isLoading, error, refetch } = useFetch("search", {
+    query: "Web Developer",
+    num_pages: 1,
+  });
+  
+  const [selectedJob, setSelectedJob] = useState(data[0]);
 
   const handleCardPress = (item) => {
     router.push(`/job-details/${item.job_id}`);
     setSelectedJob(item.job_id);
   };
-  const { data, isLoading, error, refetch } = useFetch("search", {
-    query: "React Developer",
-    num_pages: 1,
-  });
-  console.log(data);
+  
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Popular Jobs</Text>
         <TouchableOpacity>
-          <Text style={styles.headerBtn}>show all</Text>
+          <Text style={styles.headerBtn}>Show All</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.cardsContainer}>
@@ -39,15 +40,15 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4]}
-            renderItem={(item) => (
+            data={data}
+            renderItem={({ item }) => (
               <PopularJobCard
                 item={item}
                 selectedJob={selectedJob}
                 handleCardPress={handleCardPress}
               />
             )}
-            keyExtractor={(item) => item?.job_id}
+            keyExtractor={(item) => (item.job_id)}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
             showsHorizontalScrollIndicator={false}
